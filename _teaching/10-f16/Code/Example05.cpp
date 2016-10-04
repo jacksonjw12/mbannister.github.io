580fde21f9f3061cd1d56a011f6ba1a72e09cce8
Michael J. Bannister <mbannister@fastmail.fm>
2016-10-04 15:11:44 -0700

    Add lec 8

--- _teaching/10-f16/Code/Example05.cpp
+++ _teaching/10-f16/Code/Example05.cpp
@@ -14,25 +14,25 @@ int main() {
     // We start with done == false, as we assume the user want to compute at
     // at least one students grade.
     bool done = false;
-    
+
     while ( !done ) {
-        
+
         double midterm = 0;
         double homework = 0;
         double final = 0;
-        
+
         cout << "Midterm score: ";
         cin >> midterm;
         cout << "Homework score: ";
         cin >> homework;
         cout << "Final score: ";
         cin >> final;
-        
+
         double total = 0.20 * homework + 0.30 * midterm + 0.50 * final;
-        
+
         cout << "Your total score is " << total << "\n";
         cout << "Your grade for the course is ";
-        
+
         if (total > 90) {
             cout << "A\n";
         }
@@ -48,11 +48,11 @@ int main() {
         else {
             cout << "F\n";
         }
-        
+
         char response;
         cout << "Are you done (Yy/Nn)? ";
         cin >> response;
-        
+
 //        In class I used the code below, which is correct, but I have changed
 //        it to "better" code.
 //
@@ -60,29 +60,8 @@ int main() {
 //            done = true;
 //        }
         done = (response == 'Y') || (response == 'y');
-        
+
     } // end of while loop; exits when done == true.
-    
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
